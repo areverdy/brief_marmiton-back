7880a786d05c1bf0654baaa038c59def29d1136a
areverdy <alec.rev1234@gmail.com>
2023-09-14 17:03:00 +0200

    ajout premier get et boite pour bdd

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,52 @@
 import express from "express"
 import "dotenv/config"
+import { DataTypes, Model, Sequelize } from "sequelize"
+import cors from "cors"
 
 const app = express()
 const port = process.env.PORT ? parseInt(process.env.PORT as string) : 3030
+app.use(cors())
 
 app.get('/hello', (req, res) => {
   res. send('Hello World!')
 })
 
+app.get('/marmitop', (req, res) => {
+  res. send ('marmitop')
+})
+
 app. listen(port, () => {
   console.log(`Listening port on ${port}`)
 })
+const mySequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./db.sqlite",
+})
+
+const recette = mySequelize.define("nouvellerecette", {
+  nom: {
+    type: DataTypes.STRING,
+  },
+  lienimage: {
+    type: DataTypes.STRING,
+  },
+  duree: {
+    type: DataTypes.STRING,
+  },
+  note: {
+    type: DataTypes.STRING,
+  }
+}, {
+  timestamps: false,
+})
+
+
+app.post("/add/:nomrecette/:lienimage/:duree/:note", async (req, res) => {
+  recette.create({
+    nom : req.params.nomrecette, 
+    lienimage :req.params.lienimage,
+    duree : req.params.duree,
+    note : req.params.note,
+  })
+  res.sendStatus(200);
+});
